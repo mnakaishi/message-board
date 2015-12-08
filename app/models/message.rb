a1d701e21176a6eaf402882ea647a296960598ec
mnakaishi <nakaishi@mazak.it>
2015-12-08 23:05:39 +0000

    error-message

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,5 @@
 class Message < ActiveRecord::Base
     
-    validate :name , length: {maximum: 20} , presence: true
-    validate :body , length: {minimum: 2, mazimum:30} , presence: true
-    
+    validates :name , length: {maximum: 20} , presence: true
+    validates
 end
